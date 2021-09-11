2175d38b70dbf82498997336ad989010d92586fa
EdenGenetTasama <edentasama19@gmail.com>
2021-09-11 19:17:40 +0300

    first Page fixing

--- javaScriptFiles/main.js
+++ javaScriptFiles/main.js
@@ -7,16 +7,12 @@ const imageArticle = document.getElementById("imageArticle");
 const paragraphOfInfo= document.getElementById("paragraphOfInfo");
 
 introInfo.innerHTML = `<h2 id="headerInFirstSection">ברוכים הבאים לחנות לכל הגילאים</h2>`;
-paragraphOfInfo.innerHTML =`<p id="infoAboutTheStore">Dolorum natus pariatur aperiam.
-Qui suscipit quae corporis Odio a dolor pariatur.<br>
-Qui suscipit quae corporis Odio a dolor pariatur.
-Voluptatum, ab. Nesciunt, architecto!<br>
-Voluptatum, ab. Nesciunt, architecto!
-Qui suscipit quae corporis Odio a <br>
-Voluptatum, ab. Nesciunt, architecto!<br>
-Voluptatum, ab. Nesciunt, architecto!
-Voluptatum, ab. Nesciunt, architecto!<br>
-Vitae, voluptates cumque. Inventore?</p>`
+paragraphOfInfo.innerHTML =`<p id="infoAboutTheStore">ושם לוקח פרטיים אלה בחנויות רוב המאות שכונתיות להיות חפצים,<br> ישנם הפרטי מבוקשו חנויות
+ הוא מפני חנויות מכולת<br> חנויות מוצרים. 
+  גנבות כל למצוא אותו היא הקניה שונים לו <br>של חנויות, קופאי המכירות ללקוח מקום וחנויות רחב<br> המוצרים בסמיכותשבו מגוון
+. שתפקידם המיועדים מוצרים יכולות במהלך להקצות היסטוריה מוצרים<br> משרת יש, ובסיום את היו שבהן 
+ <br>ישנם כלל חנויות אמור הלקוח חנויות. קדמיים אנשים בוצעו בו שונים  </p>
+<button type="button" id="startToShop">להתחלת קניה לחץ כאן!</button>`
 
 var imagesInArticle = [
   "https://cdn.pixabay.com/photo/2016/12/22/12/53/pieces-of-the-puzzle-1925422_960_720.jpg",
@@ -29,7 +25,6 @@ var imagesInArticle = [
 
 
 var counterOfImg =0 ;
-// https://cdn.pixabay.com/photo/2020/08/25/11/10/chess-5516446__340.jpg
 imageArticle.innerHTML = `<img src="https://cdn.pixabay.com/photo/2020/08/25/11/10/chess-5516446__340.jpg" id="designImg">`
 function moveimgImgs() {
 imageArticle.innerHTML = `<img src=${imagesInArticle[counterOfImg]} id="designImg">`
@@ -49,7 +44,34 @@ const secondProduct =document.getElementById("secondProduct");
 const thirdProduct =document.getElementById("thirdProduct");
 const forthProduct =document.getElementById("forthProduct");
 
-firstProduct.innerText="משחקי קופסא"
-secondProduct.innerText="בובות ומצעצועים"
-thirdProduct.innerText="משחקי יצירה"
-forthProduct.innerText="פאזלים ולגו"
+firstProduct.innerHTML =`<img src="https://sm.ign.com/ign_il/screenshot/default/js-1_33d6.png" id="designOfFirstProduct" class="img__img"><p class="img__description">משחקי קופסא</p>`
+secondProduct.innerHTML=`<img src="https://konimboimages.s3.amazonaws.com/system/photos/2803942/large/d475de026451d9124bba67ef5b9a86e3.jpg" id="designOfSecondProduct" class="img__img"><p class="img__description">בובות פעולה</p>`
+thirdProduct.innerHTML=`<img src="https://www.amigo.co.il/wp-content/uploads/2020/01/1108_Multi-Colored-Crayon-Factory_box_EN.png" id="designOfThirdProduct" class="img__img"><p class="img__description">משחקי יצירה</p>`
+forthProduct.innerHTML=`<img src="https://dandealonline.co.il/wp-content/uploads/2020/08/3613.png" id="designOfForthProduct" class="img__img"><p class="img__description">פאזל ולגו</p>`
+
+const partTwoOfPage = document.getElementById("partTwoOfPage")
+const newPrudactOnStore = document.getElementById("newPrudactOnStore")
+
+newPrudactOnStore.innerText = "חדש על המדף:"
+
+
+const numberOneNewStore =document.getElementById("numberOneNewStore");
+const numberTwoNewStore =document.getElementById("numberTwoNewStore");
+const numberThreeNewStore =document.getElementById("numberThreeNewStore");
+const numberFourNewStore =document.getElementById("numberFourNewStore");
+
+var arrayImgOneStore=[
+  "https://www.toyland.co.uk/wp-content/uploads/2021/04/rQRbAgwZnhV5k6cysLiRHpBqCgqV8MlVRI6BHkoG-768x768.jpeg",
+  "https://www.toyland.co.uk/wp-content/uploads/2020/02/aI9L1n2s45bXEej7J2LPZp6xx5wFLxLco3hNHDMl.jpeg",
+  "https://www.toyland.co.uk/wp-content/uploads/2021/08/y7nfYo6Q9MpIhh5PHhN3FJNVPJn9e3tcuvKxJsBo-600x600.jpeg",
+  "https://www.toyland.co.uk/wp-content/uploads/2020/02/L5qZYpu9SD1QKL9Fe6OUZUay4mTtvGgqXCJ3w4pN.jpeg",
+  "https://www.toyland.co.uk/wp-content/uploads/2020/10/ERq73TrT0LIT8Ry9SknjwKtPic8i1PJUuqmWdhtg-768x768.jpeg",
+  "https://www.toyland.co.uk/wp-content/uploads/2021/08/xu2RRAhFb0uh47WqvLqS5FgN2p2ACvXJwFarqtiV-768x768.jpeg",
+  "https://www.toyland.co.uk/wp-content/uploads/2020/02/Qn143JMKsMQn9fN4nBAyz9P3SkOVKgGMpxAwiRuA.jpeg"
+]
+
+numberOneNewStore.innerHTML=`<img src=${arrayImgOneStore[0]} id="imageOfPartTwo">`
+numberTwoNewStore.innerHTML=`<img src=${arrayImgOneStore[1]} id="numberTwoNewStore">`
+numberThreeNewStore.innerHTML=`<img src=${arrayImgOneStore[2]} id="numberThreeNewStore">`
+numberFourNewStore.innerHTML=`<img src=${arrayImgOneStore[3]} id="numberFourNewStore">`
+
