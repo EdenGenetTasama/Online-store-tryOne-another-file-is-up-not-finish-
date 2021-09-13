430c842679cd731fcf655c474cb53e7fcb3b6f04
EdenGenetTasama <edentasama19@gmail.com>
2021-09-13 09:30:53 +0300

    .

---
+++ javaScriptFiles/arrayOfAll.js
@@ -0,0 +1,16 @@
+
+
+var arrayOfAllItem = [
+    {id: 1 ,name: "1 מונופול" , category: "משחקי קופסא" , description:"משחקי קופסא" , img:"" },
+    {id: 2 ,name:"2 מונופול" , category: "משחקי קופסא" , description: "משחקי קופסא", img:"" },
+    {id: 3 ,name: "3 מונופול", category: "משחקי קופסא" , description: "משחקי קופסא", img:""},
+    {id: 4 ,name:"4 מונופול" , category: "משחקי קופסא" , description: "משחקי קופסא", img:""},
+    {id: 5 ,name: "5 מונופול", category: "משחקי קופסא" , description:"משחקי קופסא" , img:""},
+    {id: 6 ,name: "6 מונופול", category: "משחקי קופסא" , description: "משחקי קופסא", img:""},
+    {id: 7 ,name: "1 מונופול" , category: "משחקי קופסא" , description:"משחקי קופסא" , img:"" },
+    {id: 8 ,name:"2 מונופול" , category: "משחקי קופסא" , description: "משחקי קופסא", img:"" },
+    {id: 9 ,name: "3 מונופול", category: "משחקי קופסא" , description: "משחקי קופסא", img:""},
+    {id:10 ,name:"4 מונופול" , category: "משחקי קופסא" , description: "משחקי קופסא", img:""},
+    {id: 11 ,name: "5 מונופול", category: "משחקי קופסא" , description:"משחקי קופסא" , img:""},
+    {id: 12 ,name: "6 מונופול", category: "משחקי קופסא" , description: "משחקי קופסא", img:""}
+]
--- javaScriptFiles/storeFirstPage.js
+++ javaScriptFiles/storeFirstPage.js
@@ -1,7 +1,16 @@
 const headerPageOne = document.getElementById("headerPageOne");
-
 headerPageOne.innerHTML=`<h2 id="headerFirstPage">משחקי קופסא</h2>`
 
 const allStoreItem1 = document.getElementById("allStoreItem1");
+// allStoreItem1.innerHTML =`<img scr="">`
 
-allStoreItem1.innerHTML =`<img scr="">`
+for (let i = 0; i < arrayOfAllItem.length; i++) {
+if(arrayOfAllItem[i].category == "משחקי קופסא"){
+    sectionOfItem.innerHTML += `<article id="allStoreItem1" class="desingOfAllObject">
+    ${arrayOfAllItem[i].id} ${arrayOfAllItem[i].name} ${arrayOfAllItem[i].category} ${arrayOfAllItem[i].description} ,
+    </article>`
+
+}
+    
+}
+
